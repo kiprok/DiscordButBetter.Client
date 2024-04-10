0830064ed7a1fd98601de5bf32cb744861b219f7
kiprok <quinn.vandenbroek@outlook.com>
2024-04-10 09:38:53 +0200

    Trying to get route params reactive

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -4,8 +4,20 @@ import MessageList from "@/components/MessageList.vue";
 import ChatTopBar from "@/components/ChatTopBar.vue";
 import {useUserStore} from "@/stores/user.js";
 import ChatTextBox from "@/components/ChatTextBox.vue";
+import {reactive, watch} from "vue";
+import {useRoute} from "vue-router";
+
+const route = useRoute()
 
 const userStore = useUserStore();
+
+const convo = reactive({convo: userStore.conversations[route.params.id]})
+
+watch(() => route.params.id, newId => {
+  convo.convo = userStore.conversations[newId]
+})
+
+
 
 </script>
 
@@ -13,12 +25,12 @@ const userStore = useUserStore();
   <div class="w-full flex flex-col flex-nowrap">
     <ChatTopBar>
       <h1 class="text-white text-3xl font-bold">
-        {{ userStore.conversations[$route.params.id].otherName }}
+        {{ convo.convo.otherName }}
       </h1>
     </ChatTopBar>
-    <message-list :messages="userStore.conversations[$route.params.id].messages"/>
+    <message-list :messages="convo.convo.messages"/>
     <div class="bg-gray-600 h-14 flex-none flex flex-row items-center px-6">
-      <ChatTextBox class="w-full" @send-chat-message="(message) => userStore.conversations[$route.params.id].messages.push(message)"/>
+      <ChatTextBox class="w-full" @send-chat-message="(message) => convo.convo.messages.push(message)"/>
     </div>
   </div>
 </template>
