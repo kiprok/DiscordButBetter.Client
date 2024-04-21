5ffbb786851825668c75ed3fab08f4fd196369ea
kiprok <quinn.vandenbroek@outlook.com>
2024-04-21 19:46:30 +0200

    Update hover background color in MessageListItem
    The hover background color for the message list item component has been changed. Previously, a border was displayed on hover; now, the background color is switched to gray instead. This provides a quicker and more obvious visual feedback for user interaction.

--- src/components/MessageListItem.vue
+++ src/components/MessageListItem.vue
@@ -51,7 +51,7 @@ function EditMessage() {
         </span>
       </div>
     </div>
-    <div class="flex flex-row justify-between group border border-black relative">
+    <div class="flex flex-row justify-between group hover:bg-gray-400 relative">
       <img :src="userStore.GetUserById(props.message.senderId).profilePicture" alt="profile picture"
            class="rounded-full size-10 ml-1 mr-2 mt-0">
       <div class="flex flex-col grow w-12">
