5e9b3bced1fc587b9e5971736a2fe56119865786
quinn <quinn.vandenBroek@outlook.com>
2024-05-24 17:34:51 +0200

    added copy function to utility.js

--- src/components/ChatTextBox.vue
+++ src/components/ChatTextBox.vue
@@ -1,62 +1,32 @@
 <script setup>
 import { useSendingMessageStore } from '@/stores/sendingMessage.js';
 import { ref, watch } from 'vue';
+import { insertAtCursor } from '@/composables/utility.js';
 const sendMessageStore = useSendingMessageStore();
 const emit = defineEmits(['SendChatMessage']);
 
-const textarea = ref(null);
-const textBox = ref(null);
+const textBox = ref();
 
 function SendChatMessage() {
   if (!sendMessageStore.messageText) return;
   emit('SendChatMessage');
 }
 
-function autoResize() {
-  if (sendMessageStore.messageText === '') {
-    textarea.value.style.height = '';
-    return;
-  }
-  if (
-    textarea.value.scrollHeight >
-    parseFloat(getComputedStyle(textarea.value).fontSize) * 1.5
-  ) {
-    textarea.value.style.height = 'auto';
-    textarea.value.style.height = `${textarea.value.scrollHeight}px`;
-  }
-}
-
 function onPaste(event) {
-  const paste = (event.clipboardData || window.clipboardData).getData('text');
+  const paste = (event.clipboardData || window.clipboardData).getData(
+    'text/plain',
+  );
 
-  // Split pasted data into lines
-  const lines = paste.split('\n');
+  document.execCommand('insertText', false, paste);
+  //insertAtCursor(paste);
 
-  // Insert each line as a separate text node
-  const selection = window.getSelection();
-  if (!selection.rangeCount) return false;
-  selection.deleteFromDocument();
-  lines.forEach((line, index) => {
-    selection.getRangeAt(0).insertNode(document.createTextNode(line));
-    if (index < lines.length - 1) {
-      selection.getRangeAt(0).insertNode(document.createElement('br'));
-    }
-  });
-  selection.collapseToEnd();
+  onInput();
 }
 
 function OnEnter(event) {
   if (event.key === 'Enter' && !event.shiftKey) {
-    const lines = event.target.innerText.split('\n');
-    const lastLine = lines[lines.length - 1];
-    const caretPos = window.getSelection().getRangeAt(0).endOffset;
-    const caretLine = window.getSelection().getRangeAt(0)
-      .startContainer.textContent;
-    console.log(caretPos, caretLine, lastLine);
-    if (caretPos === lastLine.length && caretLine === lastLine) {
-      event.preventDefault();
-      SendChatMessage();
-    }
+    event.preventDefault();
+    SendChatMessage();
   }
 }
 
@@ -65,7 +35,6 @@ watch(
   (newVal) => {
     if (textBox.value)
       if (textBox.value?.innerText !== newVal) {
-        console.log('test');
         textBox.value.innerText = newVal;
       }
   },
@@ -73,7 +42,7 @@ watch(
 );
 
 function onInput() {
-  sendMessageStore.messageText = textBox.value?.innerText;
+  sendMessageStore.messageText = textBox.value?.innerText ?? '';
 }
 </script>
 
@@ -82,14 +51,17 @@ function onInput() {
     <form class="size-full flex items-center" @submit.prevent="SendChatMessage">
       <div
         class="grow content-center h-fit w-24 min-h-8 rounded-l-lg bg-white max-h-44 overflow-y-scroll"
+        @click="textBox.focus"
       >
         <div
           contenteditable="true"
           @paste.prevent="onPaste"
           ref="textBox"
-          class="h-fit w-full break-words inline-block focus-visible:outline-none"
+          class="h-fit w-full break-words pl-2 py-2 focus-visible:outline-none"
           @keydown.enter="OnEnter"
           @input="onInput"
+          @blur="onInput"
+          id="chat-input"
         ></div>
       </div>
       <button class="flex-none px-2 w-fit h-full bg-white rounded-r-lg">
