0030ceca7988a2a4d2acd87d7f63c754b49995c3
kiprok <quinn.vandenbroek@outlook.com>
2024-06-06 23:56:00 +0200

    Added placeholder text to the chatarea

--- src/components/ChatTextBox.vue
+++ src/components/ChatTextBox.vue
@@ -66,8 +66,14 @@ function onInput() {
   <div class="size-full">
     <form class="flex size-full items-center" @submit.prevent="SendChatMessage">
       <div
-        class="h-fit max-h-44 md:max-h-96 min-h-8 w-24 grow content-center overflow-y-scroll rounded-l-lg bg-white"
+        class="relative h-fit max-h-44 md:max-h-96 min-h-8 w-24 grow content-center overflow-y-scroll rounded-l-lg
+          bg-white"
         @click="textBox.focus">
+        <div
+          v-if="sendMessageStore.messageText.trim() === ''"
+          class="absolute z-10 top-1/2 -translate-y-1/2 left-2 select-none text-black/40 truncate pointer-events-none">
+          Message @{{ conversationStore.GetConversationById(props.convoId).convoName }}
+        </div>
         <div
           contenteditable="true"
           @paste.prevent="onPaste"
