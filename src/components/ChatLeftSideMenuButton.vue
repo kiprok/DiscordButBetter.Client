bdb93155406692ef97b3236045b8dbb0187da4fd
quinn <quinn.vandenBroek@outlook.com>
2024-06-09 21:19:15 +0200

    added total notifications to menu hamburger

--- src/components/ChatLeftSideMenuButton.vue
+++ src/components/ChatLeftSideMenuButton.vue
@@ -1,17 +1,36 @@
 <script setup>
+import { useUserStore } from '@/stores/user.js';
+import { useConversationStore } from '@/stores/conversation.js';
+import { computed } from 'vue';
+import NotificationBadge from '@/components/NotificationBadge.vue';
+
+const userStore = useUserStore();
+const conversationStore = useConversationStore();
+
 function ToggleSideMenu() {
   let sideMenu = document.querySelector('#SideMenu');
   sideMenu.classList.toggle('hidden');
 }
+
+const totalNotifications = computed(() => {
+  return (
+    Object.keys(conversationStore.GetALLConversations()).reduce((acc, key) => {
+      return acc + conversationStore.GetConversationById(key).newUnseenMessages.length;
+    }, 0) + userStore.GetFriendRequests().length
+  );
+});
 </script>
 
 <template>
-  <div class="block size-8 md:hidden">
+  <div class="relative block size-8 md:hidden">
     <button
       @click="ToggleSideMenu"
       class="flex size-8 items-center justify-center text-center text-2xl text-white hover:text-gray-300">
       <i class="fa-solid fa-bars"></i>
     </button>
+    <notification-badge
+      class="absolute size-4 text-xs -top-1 -right-1"
+      :notifications="totalNotifications" />
   </div>
 </template>
 
--- src/components/NotificationBadge.vue
+++ src/components/NotificationBadge.vue
@@ -5,7 +5,8 @@ const props = defineProps(['notifications']);
 <template>
   <span
     v-if="notifications > 0"
-    class="flex items-center justify-center font-bold bg-red-600 text-white rounded-full"
+    class="flex items-center justify-center font-bold bg-red-600 text-white rounded-full select-none
+      pointer-events-none"
     >{{ Math.min(notifications, 99) }}
   </span>
 </template>
