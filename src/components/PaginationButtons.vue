77bf9013834a4a32f09e87f2a001a4720c020c7a
quinn <quinn.vandenBroek@outlook.com>
2024-06-09 21:40:04 +0200

    changed color of PaginationButtons.vue

--- src/components/PaginationButtons.vue
+++ src/components/PaginationButtons.vue
@@ -9,38 +9,39 @@ const emit = defineEmits(['setSearchPagePlace']);
 </script>
 
 <template>
-  <div class="flex-none p-1 flex flex-row items-center justify-center overflow-hidden">
+  <div class="flex-none text-black p-1 flex flex-row items-center justify-center overflow-hidden">
     <button
-      class="bg-blue-300 rounded-full size-8 flex items-center mr-1 justify-center flex-none hover:bg-blue-500
+      class="bg-gray-300 rounded-full size-8 flex items-center mr-1 justify-center flex-none hover:bg-gray-400
         cursor-pointer select-none"
       :class="{ invisible: firstPageBoundary === 1 }"
       @click="$emit('setSearchPagePlace', 1)">
       1
     </button>
     <div
-      class="size-4 flex items-center mr-1 justify-center flex-none"
+      class="size-4 flex text-white items-center mr-1 justify-center flex-none"
       :class="{ invisible: firstPageBoundary === 1 }">
       &hellip;
     </div>
     <button
       v-for="index in Math.min(lastPageNumber, 5)"
       @click="$emit('setSearchPagePlace', index + firstPageBoundary - 1)"
-      class="bg-blue-300 rounded-full size-8 flex items-center mr-1 justify-center flex-none hover:bg-blue-500
+      class="bg-gray-300 rounded-full size-8 flex items-center mr-1 justify-center flex-none hover:bg-gray-400
         cursor-pointer select-none"
       :class="{
-        '!bg-blue-600': index + firstPageBoundary - 1 === currentPlace,
+        '!bg-gray-500': index + firstPageBoundary - 1 === currentPlace,
+        '!text-white': index + firstPageBoundary - 1 === currentPlace,
       }">
       {{ index + firstPageBoundary - 1 }}
     </button>
     <div
-      class="size-4 flex items-center mr-1 justify-center flex-none"
+      class="size-4 flex text-white items-center mr-1 justify-center flex-none"
       :class="{
         invisible: lastPageBoundary >= lastPageNumber,
       }">
       &hellip;
     </div>
     <button
-      class="bg-blue-300 rounded-full size-8 flex items-center justify-center flex-none hover:bg-blue-500
+      class="bg-gray-300 rounded-full size-8 flex items-center justify-center flex-none hover:bg-gray-400
         cursor-pointer select-none"
       :class="{
         invisible: lastPageBoundary >= lastPageNumber,
